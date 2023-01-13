1bf576c173879c84a7943bc6368d0efd99843778
Erick Ochieng <erickochieng766@gmail.com>
2023-01-13 12:47:14 +0300

    Cleaned up sideeffects

--- src/components/Question.js
+++ src/components/Question.js
@@ -12,7 +12,7 @@ function Question({ question, onAnswered }) {
   useEffect(() => {
     const interval = setInterval(tick , 1000)
 
-    return () => {
+    return function cleanup() {
       clearInterval(interval);
     }
   }, [])
